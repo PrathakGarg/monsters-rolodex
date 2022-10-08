7bb4325a531f51b314169670f44f57a6f2e35f9b
Phoenix <prodotzip@gmail.com>
2022-10-09 03:48:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const App = () => {
 
 //     return (
 //       <div className="App">
-//         <h1 className="app-title">MONSTERS ROLODEX</h1>
+//         <h1 className="app-title">Monsters Rolodex</h1>
 //         <SearchBox
 //           className="monsters-search-box"
 //           onChangeHandler={onSearchChange}
